8240cc909bc0fd3052fd37a32e352999bc754497
Aleksandr Kurakin <coconutpwnz@yandex.ru>
2025-11-13 07:44:00 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://f9febfa1-4696-47d0-ab02-eca9cea43f68.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
