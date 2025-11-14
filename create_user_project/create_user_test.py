e037b53ce88ea6a77ec9e7a2abf7337ece5446e9
Aleksandr Kurakin <coconutpwnz@yandex.ru>
2025-11-14 22:51:17 +0300

    Убрал лишнее

--- create_user_project/create_user_test.py
+++ create_user_project/create_user_test.py
@@ -3,7 +3,6 @@ import configuration
 import data 
 import sender_stand_request
 
-
 def get_user_body(first_name):
     current_body = data.user_body.copy()
     current_body["firstName"] = first_name
@@ -60,7 +59,7 @@ def negative_assert_no_firstname(user_body):
     # Проверяется текст в теле ответа в атрибуте "message"
     assert response.json()["message"] == "Не все необходимые параметры были переданы"
 
-    
+
 # Тест 1. Успешное создание пользователя. Параметр firstName состоит из 2 символов
 def test_create_user_2_letter_in_first_name_get_success_response():
         positive_assert("Aa")
